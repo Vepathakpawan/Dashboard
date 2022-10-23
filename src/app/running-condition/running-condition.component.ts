f9e05bcdc2db3a5ebe097fba6593eba90f58a949
PawanVedpathak <80150887+Vepathakpawan@users.noreply.github.com>
2022-10-23 13:18:23 +0530

    stage changes push

--- src/app/running-condition/running-condition.component.ts
+++ src/app/running-condition/running-condition.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-running-condition',
   templateUrl: './running-condition.component.html',
-  styleUrls: ['./running-condition.component.scss']
+  styleUrls: ['./running-condition.component.css']
 })
 
 export class RunningConditionComponent implements OnInit {
